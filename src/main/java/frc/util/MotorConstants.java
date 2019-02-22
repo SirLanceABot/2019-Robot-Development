939aaf6d5074c091875865e3fbd090923a7c3747
= <=>
2019-02-21 21:13:41 -0500

    completed MotorConstants class -Yash

--- src/main/java/frc/util/MotorConstants.java
+++ src/main/java/frc/util/MotorConstants.java
@@ -2,13 +2,77 @@ package frc.util;
 
 public class MotorConstants
 {
-    public
+    public static double getMotorStallCurrent(Constants.MotorType motorType, double multiplier)
+    {
+        double currentLimit = 0;
+        
+        switch(motorType)
+        {
+            case kBagMotor: currentLimit = multiplier * Constants.BAG_MOTOR_STALL_CURRENT;
+                    break;
+            case kCimMotor: currentLimit = multiplier * Constants.CIM_MOTOR_STALL_CURRENT;
+                    break;
+            case k775Motor: currentLimit = multiplier * Constants.PRO_775_MOTOR_STALL_CURRENT;
+                    break;
+            case k9015Motor: currentLimit = multiplier * Constants.MOTOR_9015_STALL_CURRENT;
+                    break;
+            case kNeoMotor: currentLimit = multiplier * Constants.NEO_MOTOR_STALL_CURRENT;
+                break;
+            case kRedLine: currentLimit = multiplier * Constants.REDLINE_MOTOR_STALL_CURRENT;
+                    break;
+            default: currentLimit = 0;
+                    break;
+        }
+        return currentLimit;
+    }
+
+    public static double getMotorFreeCurrent(Constants.MotorType motorType)
+    {
+        double freeCurrent = 0;
+
+        switch(motorType)
+        {
+            case kBagMotor: freeCurrent = Constants.BAG_MOTOR_FREE_CURRENT;
+                    break;
+            case kCimMotor: freeCurrent = Constants.CIM_MOTOR_FREE_CURRENT;
+                    break;
+            case k775Motor: freeCurrent = Constants.PRO_775_MOTOR_FREE_CURRENT;
+                    break;
+            case k9015Motor: freeCurrent = Constants.MOTOR_9015_FREE_CURRENT;
+                    break;
+            case kNeoMotor: freeCurrent = Constants.NEO_MOTOR_FREE_CURRENT;
+                break;
+            case kRedLine: freeCurrent = Constants.REDLINE_MOTOR_FREE_CURRENT;
+                    break;
+            default: freeCurrent = 0;
+                    break;
+        }
+
+        return freeCurrent;
+    }
     
 
     public static class Constants
     {
+        public static enum MotorType
+        {
+            kBagMotor, kCimMotor, k775Motor, k9015Motor, kNeoMotor, kRedLine;
+        }
+        
         public static final int BAG_MOTOR_STALL_CURRENT = 41;
         public static final int CIM_MOTOR_STALL_CURRENT = 133;
-        public static final int MOTOR_775_STALL_CURRENT = 134;
+        public static final int PRO_775_MOTOR_STALL_CURRENT = 134;
+        public static final int MOTOR_9015_STALL_CURRENT = 63;
+        public static final int NEO_MOTOR_STALL_CURRENT = 150;
+        public static final int REDLINE_MOTOR_STALL_CURRENT = 134;
+
+        public static final double BAG_MOTOR_FREE_CURRENT = 1.8;
+        public static final double CIM_MOTOR_FREE_CURRENT = 2.7;
+        public static final double PRO_775_MOTOR_FREE_CURRENT = 0.7;
+        public static final double MOTOR_9015_FREE_CURRENT = 1.2;
+        public static final double NEO_MOTOR_FREE_CURRENT = 1.8;
+        public static final double REDLINE_MOTOR_FREE_CURRENT = 0.7;
+
+
     }
 }
