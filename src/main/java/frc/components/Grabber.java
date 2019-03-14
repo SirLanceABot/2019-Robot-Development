a80aa39ff030d3e06a23db3befef7c4d2cea06ab
Team 4237 Laptop 4 <lakeshoreroboticsteam@gmail.com>
2019-03-14 18:55:31 -0400

    worked on grabber state - Maxwell

--- src/main/java/frc/components/Grabber.java
+++ src/main/java/frc/components/Grabber.java
@@ -4,6 +4,9 @@ package frc.components;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import frc.control.DriverXbox;
+import frc.control.Xbox;
 
 /**
  * Add your docs here.
@@ -16,13 +19,21 @@ public class Grabber
         public static final int GRABBER_SOLENOID_RETRACT = 5;
     }
 
+    public enum grabberState
+    {
+        kRetracted, kExtended, kRumble, kNoRumble, kNone;
+    }
     
 
     private DoubleSolenoid grabberSolenoid = new DoubleSolenoid(Constants.GRABBER_SOLENOID_EXTEND,
             Constants.GRABBER_SOLENOID_RETRACT); // On the blue solenoid holder
     private Timer grabberTimer = new Timer();
+    private DriverXbox driverXbox = DriverXbox.getInstance();
     private boolean isGrabberRetracted = true;
     private boolean isGrabberMoving = false;
+    private grabberState currentGrabberState = grabberState.kRetracted;
+    private grabberState targetGrabberState = grabberState.kNone;
+    private grabberState rumbleState = grabberState.kNoRumble;
 
     private static Grabber instance = new Grabber();
 
@@ -90,5 +101,77 @@ public class Grabber
     {
         return grabberSolenoid.get();
     }
-    
+
+    public void setState(grabberState targetGrabberState)
+    {
+        this.targetGrabberState = targetGrabberState;
+    }
+
+    public void grabberControl()
+    {
+        switch(targetGrabberState)
+        {
+            case kExtended:
+                if(currentGrabberState == grabberState.kRetracted)
+                {
+                    extendGrabber();
+                    grabberTimer.reset();
+                    grabberTimer.start();
+                    currentGrabberState = grabberState.kExtended;
+                }
+                else if(currentGrabberState == grabberState.kExtended && grabberTimer.get() > 5.0)
+                {
+                    currentGrabberState = grabberState.kExtended;
+                    rumbleState = grabberState.kRumble;
+                }
+                else if(currentGrabberState == grabberState.kNone)
+                {
+                    extendGrabber();
+                    grabberTimer.reset();
+                    grabberTimer.start();
+                    currentGrabberState = grabberState.kExtended;
+                }
+                else if(currentGrabberState == grabberState.kExtended)
+                {
+                    currentGrabberState = grabberState.kExtended;
+                }
+                break;
+            case kRetracted:
+                if(currentGrabberState == grabberState.kExtended)
+                {
+                    retractGrabber();
+                    grabberTimer.stop();
+                    grabberTimer.reset();
+                    currentGrabberState = grabberState.kRetracted;
+                    rumbleState = grabberState.kNoRumble;
+                }
+                else if(currentGrabberState == grabberState.kNone)
+                {
+                    retractGrabber();
+                    currentGrabberState = grabberState.kRetracted;
+                    rumbleState = grabberState.kNoRumble;
+                }
+                else if(currentGrabberState == grabberState.kRetracted)
+                {
+                    currentGrabberState = grabberState.kRetracted;
+                    rumbleState = grabberState.kNoRumble;
+                }
+                break;
+            case kNone:
+                break;
+        }
+    }
+        public void setRumble()
+        {
+            if(rumbleState == grabberState.kRumble)
+            {
+                driverXbox.setRumble(RumbleType.kRightRumble, 0.5);;
+            }
+            else
+            {
+                driverXbox.setRumble(RumbleType.kRightRumble, 0.00);
+            }
+
+        }
+
 }
